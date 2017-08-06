0ab34baa9dd4ce03137d23ae9a22507ccc8eedcb
Iván Sánchez Ortega <ivan@sanchezortega.es>
2017-08-06 21:25:35 +0200

    File mode change

--- src/api.js
+++ src/api.js
@@ -4,7 +4,7 @@ import * as oqtm from './gdgg/oqtm.js';
 import * as fucks from './encoders/fucks.js';
 import * as goshdarnits from './encoders/goshdarnits.js';
 import * as pokes from './encoders/pokes.js';
-import * as ikea  from './encoders/ikea.js';
+// import * as ikea  from './encoders/ikea.js';
 import * as tokiPonaSyllabes from './encoders/toki-pona-syllabes.js';
 import * as cheeses from './encoders/cheese.js';
 
@@ -15,7 +15,7 @@ export var encoders = {
 	fucks: fucks,
 	goshdarnits: goshdarnits,
 	pokes: pokes,
-	ikea: ikea,
+// 	ikea: ikea,
 	tokiPonaSyllabes: tokiPonaSyllabes,
 	cheeses: cheeses
 }
